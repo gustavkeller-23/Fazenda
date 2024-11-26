c9b03efa29dee05b41bade2a0fcd2db0c81f5ce5
Gustav Adolf Lutz Keller <124906037+gustavkeller-23@users.noreply.github.com>
2024-11-26 10:01:14 -0300

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany to javafx.fxml;
-    exports com.mycompany;
-    requires org.mongodb.driver.core;
-    requires java.base;
-}
